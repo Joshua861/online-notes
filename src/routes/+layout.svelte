4824cb14a9b85e8b95299fa70580091403449e30
Joshua861 <joshrl@proton.me>
2024-04-07 11:33:28 +0100

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,6 +22,14 @@
 	});
 </script>
 
+<svelte:head>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+	<meta name="theme-color" content="#111213" />
+</svelte:head>
+
 <Toaster theme="dark" />
 <slot />
 <div class="h-10"></div>
